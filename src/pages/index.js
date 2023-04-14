a179e877fd1411d97a44c84e1c2eabc0fd03c16b
Cinquin Andy <andy.cinquin@infotel.com>
2023-04-14 16:15:16 +0200

    clean index start the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 
 import Map from '@components/Map'
+import React from 'react'
+import Image from 'next/image'
 
 const DEFAULT_CENTER = [38.907132, -77.036546]
 
@@ -13,7 +15,28 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<section className={'h-screen w-screen'}>
+			<section className={'flex h-screen w-screen'}>
+				<div
+					className={'flex h-full w-1/5 flex-col bg-sky-950 p-10 text-slate-50'}
+				>
+					<div className={'flex w-full items-center justify-center'}>
+						<Image
+							src={'/telecommuting.svg'}
+							width={'35'}
+							height={'35'}
+							alt={'logo'}
+						></Image>
+					</div>
+					<h2
+						className={
+							'mt-3 flex items-center justify-center text-center text-xs'
+						}
+					>
+						TaN
+						<br />
+						Dataviz
+					</h2>
+				</div>
 				<Map width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
 					{({ TileLayer, Marker, Popup }) => (
 						<>
