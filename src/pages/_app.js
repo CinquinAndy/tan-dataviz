1c54f2fd0a8d2cb2f90abc552e01e28413cc6390
Cinquin Andy <andy.cinquin@infotel.com>
2023-05-03 15:14:33 +0200

    display markers on map

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
-export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+const queryClient = new QueryClient()
+
+export default function App({
+                                Component,
+                                pageProps: {session, ...pageProps},
+                            }) {
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </Head>
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </>
+    )
 }
