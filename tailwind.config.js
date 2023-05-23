b77f2c071039cdbc8669a1b13898a3a6d256b5d2
CinquinAndy <cinquin.andy@gmail.com>
2023-05-23 17:47:33 +0200

    oupsi, that was case sensive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx}',
-		'./src/components/**/*.{js,ts,jsx,tsx}',
+		'./src/Components/**/*.{js,ts,jsx,tsx}',
 		'./src/app/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
